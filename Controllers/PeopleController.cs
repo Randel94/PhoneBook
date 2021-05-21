16af059e7b568d4fc62072739aa73e1216e0a531
Randel94 <korotkih.smak@mail.ru>
2021-05-21 13:14:28 +0700

    Убрал комменты

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -92,8 +92,6 @@ namespace PhoneBook.Controllers
         }
 
         // POST: People/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Name,SName,BDate")] Person person, [Bind("Number")] PhoneNumber phoneNumber)
@@ -126,8 +124,6 @@ namespace PhoneBook.Controllers
         }
 
         // POST: People/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> EditPost(int? id)
--- Controllers/PhoneNumbersController.cs
+++ Controllers/PhoneNumbersController.cs
@@ -19,14 +19,6 @@ namespace PhoneBook.Controllers
             _context = context;
         }
 
-        /*
-        // GET: PhoneNumbers
-        public async Task<IActionResult> Index()
-        {
-            var pBContext = _context.PhoneNumbers.Include(p => p.Person);
-            return View(await pBContext.ToListAsync());
-        }
-        */
         // GET: PhoneNumbers/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -46,33 +38,7 @@ namespace PhoneBook.Controllers
             return View(phoneNumber);
         }
 
-        /*
         // GET: PhoneNumbers/Create
-        public IActionResult Create()
-        {
-            ViewData["PersonId"] = new SelectList(_context.People, "Id", "Name");
-            return View();
-        }
-
-        // POST: PhoneNumbers/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Number")] PhoneNumber phoneNumber, int? Id)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(phoneNumber);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["PersonId"] = new SelectList(_context.People, "Id", "Name", phoneNumber.PersonId);
-            return View(phoneNumber);
-        }
-        */
-
-        //Добавление номера телефона.
         public IActionResult Create(int? id)
         {
             if (id != null)
@@ -82,6 +48,7 @@ namespace PhoneBook.Controllers
             return NotFound();
         }
 
+        // POST: PhoneNumbers/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Number", "PersonId")] PhoneNumber phoneNumber)
@@ -109,46 +76,10 @@ namespace PhoneBook.Controllers
             {
                 return NotFound();
             }
-            //ViewData["PersonId"] = new SelectList(_context.People, "Id", "Name", phoneNumber.PersonId);
             return View(phoneNumber);
         }
-        /*
-        // POST: PhoneNumbers/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("Number")] PhoneNumber phoneNumber)
-        {
-            if (id != phoneNumber.Number)
-            {
-                return NotFound();
-            }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(phoneNumber);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PhoneNumberExists(phoneNumber.Number))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["PersonId"] = new SelectList(_context.People, "Number", phoneNumber.Number);
-            return View(phoneNumber);
-        }
-        */
+        // POST: PhoneNumbers/Edit/5
         [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> EditPost(int? id)
